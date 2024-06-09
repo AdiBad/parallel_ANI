626154d0f9aed503390bf9384cb02491493b7291
adibad <badola.adi@gmail.com>
2024-06-09 04:45:43 +0200

    formalize test case

--- tests/test_calcluate_ani.py
+++ tests/test_calcluate_ani.py
@@ -10,12 +10,13 @@ from parallel_ANI.calculate_ani import ani_score
 
 
 class TestCalculateANI(unittest.TestCase):
+
     def setUp(self):
         self.fasta_files = {
             2: r'D:\CodingProjects\parallel_ANI\fasta_data\test_2.fasta',
             3: r'D:\CodingProjects\parallel_ANI\fasta_data\test_3.fasta',
         }
-        self.expected_answers = {
+        self.expected_scores = {
             2: 75.0,
             3: 50.0
         }
@@ -23,9 +24,8 @@ class TestCalculateANI(unittest.TestCase):
     @parameterized.expand([(2), (3)])
     def test_ani_score(self, file_2: int):
         fasta_file_1 = r'D:\CodingProjects\parallel_ANI\fasta_data\test_1.fasta'
-        testing_file = self.fasta_files[file_2]
         assert ani_score(
-            fasta_file_1, testing_file) == self.expected_answers[file_2]
+            fasta_file_1, self.fasta_files[file_2]) == self.expected_scores[file_2]
 
 
 if __name__ == '__main__':
